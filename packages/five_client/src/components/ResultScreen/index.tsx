6e67bae18e55d629ae86d50753358b0c91b75b30
aho4ahoaho <aho4ahoaho@outlook.jp>
2023-10-27 01:31:57 +0900

    スタイルの変更

--- packages/five_client/src/components/ResultScreen/index.tsx
+++ packages/five_client/src/components/ResultScreen/index.tsx
@@ -20,6 +20,7 @@ export const ResultScreen = ({
                 <div className={style.result}>{isWin ? '勝利' : '敗北'}</div>
                 <div className={style.winner}>勝者:{winner_name}</div>
                 <input
+                    className={style.button}
                     type="button"
                     value="メニューに戻る"
                     onClick={onBackToMenu}
